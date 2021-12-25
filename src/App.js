f43bc0e9200f6222309df08e37c009fc28d79d6a
Wonn <wonndev@gmail.com>
2021-12-26 03:09:49 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World with React.js
+          Hello World with React.js (w)
         </p>
         <a
           className="App-link"
