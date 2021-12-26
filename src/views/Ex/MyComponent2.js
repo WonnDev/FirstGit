2d0d083be91b896f86d6f33f28ebb224f1bd197e
Wonn <wonndev@gmail.com>
2021-12-27 05:08:25 +0700

    4+5 kbb

--- src/views/Ex/MyComponent2.js
+++ src/views/Ex/MyComponent2.js
@@ -1,9 +1,26 @@
 import React from "react";
 
 class MyComponent2 extends React.Component{
+    
+    //key
+    state = {
+        name: 'W',
+        age: '18',
+    }
+
     render(){
+        
+        let add = 'PT';  
+
         return(
-            <div>This test class component</div>
+            //fragnent      
+            <> 
+            <div className="first">This test class component</div>
+            <div className="second">test {add}</div>
+                <div className="second">test {this.state.name}</div>
+                <div className="second">test {this.state['age']}</div>
+            <div className="third">{console.log("HTML")}</div>
+            </>
         )
     }
 }
