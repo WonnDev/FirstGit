f785d74865d2335729f778a3dc0b11e2fa229037
Wonn <wonndev@gmail.com>
2022-04-03 15:19:21 +0700

    fist

--- src/views/Ex/Home.js
+++ src/views/Ex/Home.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
-class Home extends React.Component{
-    componentDidMount(){
+import logo from '../../assets/images/meo.jpg'
+
+
+class Home extends React.Component {
+    componentDidMount() {
         // setTimeout(()=>{
         //     this.props.history.push('/todo')
         // },5000)
     }
     render() {
-        return(
-            <div>Hello from Homepage</div>
+        return (
+            <>
+                <div>Hello from Homepage</div>
+                <div>Wellcome to MyReactWeb</div>
+                <div>
+                    <img src={logo} />
+                </div>
+            </>
         )
     }
 }
